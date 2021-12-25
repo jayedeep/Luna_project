7bc355714d45aaaf3fcd6b192320b5aa1c620e2f
jayedeep <jaydeeptalaviya7@gmail.com>
2021-12-25 13:45:45 +0530

    make it better 17

--- blog_reactify/urls.py
+++ blog_reactify/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
-urlpatterns+=[path('',include('frontend.urls'))]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=[path('',include('frontend.urls'))]
