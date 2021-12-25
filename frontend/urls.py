a7d7cb5adeb2994ba3590d0b393c44e5c46b3bb9
jayedeep <jaydeeptalaviya7@gmail.com>
2021-12-25 17:09:54 +0530

    final commit first

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,6 @@ from .views import index  # the view responsible for the frontend
 
 urlpatterns = [
     path('', index,name='index'),  # add the view to the url
-    re_path(r'^.*$', index,name='index'), # saved my day
+    re_path(r'^(?:.*)/?$', index,name='index'), # saved my day
 
 ]
