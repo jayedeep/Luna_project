8c39ff3a8d2c25ef414e9f7506da87aa8c6a725e
jayedeep <jaydeeptalaviya7@gmail.com>
2021-12-25 12:41:43 +0530

    make it better 12

--- blog_reactify/settings.py
+++ blog_reactify/settings.py
@@ -219,7 +219,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA_DIR = BASE_DIR / 'media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
